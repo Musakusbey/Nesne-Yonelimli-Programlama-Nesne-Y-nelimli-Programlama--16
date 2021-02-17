cce5bbd4823a2415bc85490a8c4377e8f54167cd
Musa Kuşbey <78588618+Musakusbey@users.noreply.github.com>
2021-02-17 15:41:17 +0300

    Add files via upload
    Nesne Yonelimli Programlama (Nesne Yönelimli Programlama)-- 16->Fonksiyonların veya metodların aldığı parametreler

---
+++ 16 Fonksiyonların veya metodların aldığı parametreler.cpp
@@ -0,0 +1,88 @@
+/*#include<iostream>
+
+using namespace std;
+
+void degerilegecir(int yenisayi)
+{
+	yenisayi=42;
+}
+                                  //1.si deger ile gecirmek
+int main ()
+{
+	int sayi=3;
+	
+	degerilegecir(sayi);
+	
+	std::cout<<"sayi="<<sayi<<std::endl;
+	
+	return 0;
+} */
+
+/*#include<iostream>
+
+using namespace std;
+
+void referanslagecir(int &yenisayi)
+{
+	yenisayi=42;
+}
+                                             //2.si referans ile geçirmek
+int main ()
+{
+	int sayi=3;
+	
+	referanslagecir(sayi);
+	
+	std::cout<<"sayi="<<sayi<<std::endl;
+	
+	return 0;
+}
+  */
+  
+  
+/*#include<iostream>
+
+using namespace std;
+
+void isaretciilegecir(int*yenisayi)
+{
+	*yenisayi=42;
+}
+
+int main ()
+{
+	int sayi=3;
+	
+	isaretciilegecir(&sayi);
+	
+	std::cout<<"sayi="<<sayi<<std::endl;
+	
+	return 0;
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
